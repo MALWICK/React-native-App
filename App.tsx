f3a076823e56d0fac10a884d7c42196f78b62e19
MALWICK <batahumphrey66@gamil.com>
2023-12-16 16:36:59 +0100

    setting up the repo

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.box}>
+        <Text style={styles.text}>1</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text}>2</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap:10,
   },
+  box: {
+    backgroundColor: "red",
+    width: 300,
+    height: 300,
+  },
+  text: {
+    color: "blue",
+    fontSize: 50,
+  },
+
 });
